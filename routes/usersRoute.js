3dff4700efcf23778a3b72102781198743866b56
SU-T <guitaristpeet@gmail.com>
2025-01-07 22:14:21 -0500

    GET single user route

--- routes/usersRoute.js
+++ routes/usersRoute.js
@@ -2,9 +2,18 @@ const express = require("express");
 const router = express.Router();
 
 const users = require("../data/users");
+// const user = users.find((user) => user.user_id === 1);
+// console.log(user);
 
 router.get("/", (req, res) => {
   res.status(200).json(users);
+});
+
+router.get("/:id", (req, res, next) => {
+  const user = users.find((user) => user.user_id.toString() === req.params.id);
+  if (user) {
+    res.status(200).json(user);
+  } else next();
 });
 
 module.exports = router;
