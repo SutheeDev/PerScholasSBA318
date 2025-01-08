a72ed4a59a407009d15124897cef32613b682484
SU-T <guitaristpeet@gmail.com>
2025-01-07 21:18:56 -0500

    added error handling middleware

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const app = express();
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  //   res.send("Hello World");
+  throw new Error("test the error middleware");
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  res.status(err.status || 500).send("Something went wrong, try again later.");
 });
 
 app.listen(port, () => {
